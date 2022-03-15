3efb7b9cfa6031cce9bf93ffb415f5eedcebec45
NMCannon <nmichaelcannon@gmail.com>
2022-03-15 22:52:35 +0000

    added sidebar with twitter timeline embed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div id="sidebar-container">
+        <div id="sidebar">
+            <a className="twitter-timeline" data-width="350" data-height="720" data-theme="dark" href="https://twitter.com/DCPoliceDept?ref_src=twsrc%5Etfw">Tweets by DCPoliceDept</a> <script async src="https://platform.twitter.com/widgets.js"></script>
+        </div>
+    </div>
+  )
+}

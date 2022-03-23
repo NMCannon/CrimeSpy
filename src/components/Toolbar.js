e97589ed21945a53cd257b9652a0f7a6ebbf7dfd
NMCannon <nmichaelcannon@gmail.com>
2022-03-23 20:21:32 +0000

    made toolbar sticky, UI tweaks

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -26,38 +26,40 @@ class Toolbar extends Component {
 
   render() {
     return (
-      <div id="toolbar">
-        <Button text="Map"/>
-        <Button text="Statistics"/>
+      <div id="toolbar-container">
+        <div id="toolbar">
+          <Button text="Map"/>
+          <Button text="Statistics"/>
 
-        <div className="d-flex">
-          <div className="vr" style={{currentColor: 'white'}}></div>
+          <div className="d-flex">
+            <div className="vr" style={{currentColor: 'white'}}></div>
+          </div>
+
+          <DropdownButton className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.dropDownValueYear}>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>ALL</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2022</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2021</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2020</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2019</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2018</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2017</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2016</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2015</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2014</div></Dropdown.Item>
+          </DropdownButton>
+
+          <DropdownButton  className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.dropDownValueCrime}>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ALL</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ASSAULT</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>BURGLARY</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>HOMICIDE</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>MOTOR VEHICLE THEFT</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ROBBERY</div></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>SEXUAL ABUSE</div></Dropdown.Item>
+          </DropdownButton>
+
+          <button id="apply-btn" onClick={this.click}>Apply</button>
         </div>
-
-        <DropdownButton className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.dropDownValueYear}>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>ALL</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2022</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2021</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2020</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2019</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2018</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2017</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2016</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2015</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2014</div></Dropdown.Item>
-        </DropdownButton>
-
-        <DropdownButton  className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.dropDownValueCrime}>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ALL</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ASSAULT</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>BURGLARY</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>HOMICIDE</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>MOTOR VEHICLE THEFT</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ROBBERY</div></Dropdown.Item>
-          <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>SEXUAL ABUSE</div></Dropdown.Item>
-        </DropdownButton>
-
-        <button id="apply-btn" onClick={this.click}>Apply</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
 
 // Render App.js on root div
 ReactDOM.render(
@@ -18,5 +16,22 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {addSticky()};
+
+// Get the toolbar
+let toolbar = document.getElementById("toolbar");
+
+// Get the offset position of the toolbar
+let sticky = toolbar.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function addSticky() {
+  if (window.pageYOffset > sticky) {
+    toolbar.classList.add("sticky");
+  } else {
+    toolbar.classList.remove("sticky");
+  }
+}
 
 reportWebVitals();

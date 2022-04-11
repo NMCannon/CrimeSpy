0b434b58af1bf9ea5bccbde9b65a65a884ae1d92
NMCannon <nmichaelcannon@gmail.com>
2022-04-11 20:19:57 +0100

    removed hrefs from toolbar.js, changed button colours

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -10,7 +10,7 @@ class Toolbar extends Component {
     this.state = {
       year: "Select Year",
       crime: "Select Crime",
-      stattype: "Statistics",
+      stattype: "Chart",
       map: false
     }
   }
@@ -41,8 +41,8 @@ class Toolbar extends Component {
       <div id="toolbar-container">
         <div id="toolbar">
           <DropdownButton  className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.stattype}>
-            <Dropdown.Item href="#/action-2"><div onClick={this.mapChangeFalse}>Chart</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={this.mapChangeTrue}>Map</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={this.mapChangeFalse}>Chart</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={this.mapChangeTrue}>Map</div></Dropdown.Item>
           </DropdownButton>
 
           <div className="d-flex">
@@ -50,26 +50,26 @@ class Toolbar extends Component {
           </div>
 
           <DropdownButton className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.year}>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>ALL</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2022</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2021</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2020</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2019</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2018</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2017</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2016</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2015</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValue(e.target.textContent)}>2014</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>ALL</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2022</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2021</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2020</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2019</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2018</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2017</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2016</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2015</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValue(e.target.textContent)}>2014</div></Dropdown.Item>
           </DropdownButton>
 
           <DropdownButton  className="dropdown-basic-button" id="year-dropdown" menuVariant="dark" drop="right" title={this.state.crime}>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ALL</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ASSAULT</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>BURGLARY</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>HOMICIDE</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>MOTOR VEHICLE THEFT</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ROBBERY</div></Dropdown.Item>
-            <Dropdown.Item href="#/action-2"><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>SEXUAL ABUSE</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ALL</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ASSAULT</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>BURGLARY</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>HOMICIDE</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>MOTOR VEHICLE THEFT</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>ROBBERY</div></Dropdown.Item>
+            <Dropdown.Item><div onClick={(e) => this.changeValueCrime(e.target.textContent)}>SEXUAL ABUSE</div></Dropdown.Item>
           </DropdownButton>
 
           <button id="apply-btn" onClick={this.applyFilter}>Apply</button>
